9d0feb687324846a04c57b32640100271351aae9
Anton Boyko <me@boykoant.pro>
2019-11-04 19:04:58 +0200

    continued with nodejs docker project

--- nodeJS/03-docker/publish.sh
+++ nodeJS/03-docker/publish.sh
@@ -1,2 +1,13 @@
-func azure functionapp publish \
-    ""
+ACR=jsTalks201903
+
+docker login \
+    -u $ACR \
+    $ACR.azurecr.io
+
+docker build \
+    -t $ACR.azurecr.io/function-node-x86 \
+    -f Dockerfile-x86 \
+    .
+
+docker push \
+    $ACR.azurecr.io/function-node-x86
