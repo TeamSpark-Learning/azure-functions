65cad098b6a446777bed93c13017fff85b764f27
Anton Boyko <me@boykoant.pro>
2019-11-05 11:45:57 +0200

    cleanup

--- nodeJS/02-durable/orchestrator-sequential/index.js
+++ nodeJS/02-durable/orchestrator-sequential/index.js
@@ -1,12 +1,6 @@
 ﻿/*
  * This function is not intended to be invoked directly. Instead it will be
  * triggered by an HTTP starter function.
- * 
- * Before running this sample, please:
- * - create a Durable activity function (default name is "Hello")
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your 
- *    function app in Kudu
  */
 
 const df = require("durable-functions");
