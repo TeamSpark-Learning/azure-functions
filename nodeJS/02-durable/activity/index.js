c7a02441f3780868dd5f066cb03765ef57bfad6f
Anton Boyko <me@boykoant.pro>
2019-11-05 11:46:34 +0200

    continued with nodejs durable project

--- nodeJS/02-durable/activity/index.js
+++ nodeJS/02-durable/activity/index.js
@@ -7,10 +7,13 @@ let doWork = async function(args) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(args);
-        }, 3000);
+        }, 3 * 1000);
     });
 }
 
 module.exports = async function (context) {
-    return await doWork(context.bindings.name);
+    let result = await doWork(context.bindings.name);
+    
+    context.log("=================================>", result);
+    return result;
 };
